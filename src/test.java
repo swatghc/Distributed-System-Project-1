3ec65ff9ca9d792278226749ce8a3344ab6eb0be
swatghc <447099673@qq.com>
2017-03-22 22:55:44 +1100

    3rd push

--- src/test.java
+++ src/test.java
@@ -1,9 +1,13 @@
+import com.sun.glass.ui.SystemClipboard;
+
 public class test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("This is the main class of this project");
 		System.out.println("how about this");
+		
+		System.out.println("new push");
 	}
 
 }
